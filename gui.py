bd5e8f293e5a89aa8010d63c5e6a51a587cb56b1
Nathan Harrison <52888031+nateCode1@users.noreply.github.com>
2024-02-01 22:41:52 -0500

    Functional Title Text + GUI

--- gui.py
+++ gui.py
@@ -4,13 +4,21 @@ from tkinter import *
 import json
 from screeninfo import get_monitors
 from PIL import Image, ImageTk
-from main import composite_image
+from main import composite_image, set_background
+
+
+def on_close():
+    set_background()
+    root.destroy()
 
 class XKCDBackgroundTool:
     def __init__(self, master):
         self.target_preview_size = (0,0)
         self.master = master
         self.master.title("XKCD Background Tool")
+
+        with open("config.json", 'r') as file:
+            config = json.load(file)
 
         # Set styles
         large_font_bold = ('Lucida', 18)
@@ -26,13 +34,13 @@ class XKCDBackgroundTool:
 
         # Mode selection (Radio buttons)
         self.mode_label = ttk.Label(master, text="Select Mode:")
-        self.mode_var = tk.StringVar(value="Random")
+        self.mode_var = tk.StringVar(value=config["mode"])
         self.random_radio = ttk.Radiobutton(master, text="Random", variable=self.mode_var, value="Random")
         self.latest_radio = ttk.Radiobutton(master, text="Latest", variable=self.mode_var, value="Latest")
 
         # Run option selection (Radio buttons)
         self.run_label = ttk.Label(master, text="Select Run Option:")
-        self.run_var = tk.StringVar(value="Run on Startup")
+        self.run_var = tk.StringVar(value=config["run"])
         self.startup_radio = ttk.Radiobutton(master, text="Run on Startup", variable=self.run_var,
                                              value="Run on Startup")
         self.everyday_radio = ttk.Radiobutton(master, text="Run Every Day", variable=self.run_var,
@@ -40,19 +48,24 @@ class XKCDBackgroundTool:
 
         # Background color selection
         self.color_label = ttk.Label(master, text="Select Background Color:")
-        self.color_var = tk.StringVar(value="#FFFFFF")
+        self.color_var = tk.StringVar(value=config["color"])
         self.color_button = ttk.Button(master, text="Choose Color", command=self.choose_color)
 
+        # Text color selection
+        self.text_color_label = ttk.Label(master, text="Select Text Color:")
+        self.text_color_var = tk.StringVar(value=config["text_color"])
+        self.text_color_button = ttk.Button(master, text="Choose Color", command=self.choose_text_color)
+
         # Toggle drop shadow
-        self.shadow_var = tk.BooleanVar(value=False)
+        self.shadow_var = tk.BooleanVar(value=config["shadow"])
         self.shadow_checkbox = ttk.Checkbutton(master, text="Enable Drop Shadow?", variable=self.shadow_var)
 
         # Toggle for displaying alt text
-        self.alt_text_var = tk.BooleanVar(value=True)
+        self.alt_text_var = tk.BooleanVar(value=config["alt_text"])
         self.alt_text_checkbox = ttk.Checkbutton(master, text="Display Alt Text", variable=self.alt_text_var)
 
         # Toggle for displaying title
-        self.title_var = tk.BooleanVar(value=True)
+        self.title_var = tk.BooleanVar(value=config["title"])
         self.title_checkbox = ttk.Checkbutton(master, text="Display Title", variable=self.title_var)
 
         # Regenerate the preview
@@ -76,17 +89,21 @@ class XKCDBackgroundTool:
         self.color_label.grid(row=2, column=0, padx=10, pady=5, sticky="w")
         self.color_button.grid(row=2, column=1, padx=10, pady=5, sticky="w")
 
-        self.shadow_checkbox.grid(row=3, column=0, columnspan=2, padx=10, pady=5, sticky="w")
-        self.alt_text_checkbox.grid(row=3, column=1, padx=10, pady=5, sticky="w")
-        self.title_checkbox.grid(row=3, column=2, padx=10, pady=5, sticky="w")
+        self.text_color_label.grid(row=3, column=0, padx=10, pady=5, sticky="w")
+        self.text_color_button.grid(row=3, column=1, padx=10, pady=5, sticky="w")
 
-        self.preview_label.grid(row=4, column=0, columnspan=3, padx=10, pady=5, sticky="w")
-        self.preview_image.grid(row=5, column=0, columnspan=3, padx=10, pady=5)
+        self.shadow_checkbox.grid(row=4, column=0, columnspan=2, padx=10, pady=5, sticky="w")
+        self.alt_text_checkbox.grid(row=4, column=1, padx=10, pady=5, sticky="w")
+        self.title_checkbox.grid(row=4, column=2, padx=10, pady=5, sticky="w")
+
+        self.preview_label.grid(row=5, column=0, columnspan=3, padx=10, pady=5, sticky="w")
+        self.preview_image.grid(row=6, column=0, columnspan=3, padx=10, pady=5)
 
         # Add traces to variables to call the update function on change
-        self.mode_var.trace_add("write", self.update)
-        self.run_var.trace_add("write", self.update)
+        self.mode_var.trace_add("write", lambda *args : self.update(*args, trigger_rerender=False))
+        self.run_var.trace_add("write", lambda *args : self.update(*args, trigger_rerender=False))
         self.color_var.trace_add("write", self.update)
+        self.text_color_var.trace_add("write", self.update)
         self.shadow_var.trace_add("write", self.update)
         self.alt_text_var.trace_add("write", self.update)
         self.title_var.trace_add("write", self.update)
@@ -96,9 +113,14 @@ class XKCDBackgroundTool:
         if color_code:
             self.color_var.set(color_code)
 
+    def choose_text_color(self):
+        color_code = colorchooser.askcolor()[1]
+        if color_code:
+            self.text_color_var.set(color_code)
+
     def update_image(self):
         # Re-render
-        composite_image("comic.png",)
+        composite_image()
 
         # Scale the image to fit in the preview
         res = get_monitors()[0]
@@ -110,11 +132,12 @@ class XKCDBackgroundTool:
         resized_image = original_image.resize(self.target_preview_size, Image.LANCZOS)
         resized_image.save("preview.png")
 
-    def update(self, *args):
+    def update(self, *args, trigger_rerender=True):
         config_data = {
             "mode": self.mode_var.get(),
             "run": self.run_var.get(),
             "color": self.color_var.get(),
+            "text_color": self.text_color_var.get(),
             "shadow": self.shadow_var.get(),
             "alt_text": self.alt_text_var.get(),
             "title": self.title_var.get()
@@ -124,7 +147,16 @@ class XKCDBackgroundTool:
         with open("config.json", "w") as json_file:
             json.dump(config_data, json_file)
 
+        # Re-render the image
+        if trigger_rerender:
+            self.update_image()
+            new_image = tk.PhotoImage(file="preview.png")
+            self.preview_image.configure(image=new_image)
+            self.preview_image.image = new_image
+
+
 if __name__ == "__main__":
     root = tk.Tk()
+    root.protocol("WM_DELETE_WINDOW", on_close)
     app = XKCDBackgroundTool(root)
     root.mainloop()
--- main.py
+++ main.py
@@ -1,19 +1,62 @@
+import textwrap
+
 import requests
 from bs4 import BeautifulSoup as bs
 import ctypes
-from PIL import Image, ImageDraw, ImageFilter
+from PIL import Image, ImageDraw, ImageFilter, ImageFont
 import os
 from screeninfo import get_monitors
+import json
 
 
 def add_tuples(tup1, tup2):
     return tuple([tup1[i] + tup2[i] for i in range(len(tup1))])
 
 
-def get_image(image_path):
+def draw_text(text, size, ypos, fill_color, background):
+    # Create a drawing object
+    draw = ImageDraw.Draw(background)
+
+    # Load a font
+    font = ImageFont.truetype("Fonts/LucidaSansDemiBold.ttf", size)
+
+    # Determine the characters per line
+    letter_size = draw.textbbox((0, 0), 'a', font)
+    max_line_length = background.width // letter_size[1] - 5
+
+    wrapped_text = textwrap.fill(text, width=max_line_length)
+
+    # Calculate the width and height of the text
+    text_size = draw.textbbox((0, 0), wrapped_text, font)
+
+    # Calculate the position to center the text
+    x = (background.width - text_size[0]) // 2 - text_size[2] // 2
+    y = ypos  # (background.height - text_size[1]) // 2
+
+    # Add text to the image
+    # draw.text((x, y), text, font=font, fill=(0, 0, 0), align="center")
+    draw.multiline_text((x, y), wrapped_text, font=font, fill=fill_color, align="center")
+
+
+def color_from_hex(hex_string):
+    hex_string = hex_string.lstrip('#')
+
+    # Use int function with base 16 to convert hex to integer
+    r = int(hex_string[0:2], 16)
+    g = int(hex_string[2:4], 16)
+    b = int(hex_string[4:6], 16)
+
+    # Return the 3-tuple of integers
+    return r, g, b
+
+
+def get_image():
+    image_path = "comic.png"
     r = requests.get('https://c.xkcd.com/random/comic/')
 
     soup = bs(r.content, features="lxml")
+
+    # Image
     images = soup.select('#comic img')
     images_url = images[0]['src']
     img_data = requests.get('https:' + images_url).content
@@ -21,13 +64,30 @@ def get_image(image_path):
     with open(image_path, 'wb') as handler:
         handler.write(img_data)
 
+    # Metadata
+    title = soup.select('#ctitle')[0].get_text()
+    alt_text = images[0]['title']
 
-def composite_image(image_path, max_img_width_pct=0.9, max_img_height_pct=0.75):
+    image_data = {
+        'title': title,
+        'alt_text': alt_text
+    }
+
+    with open("image_meta.json", 'w') as file:
+        json.dump(image_data, file, indent=2)
+
+
+def composite_image(max_img_width_pct=0.9, max_img_height_pct=0.7):
+    image_path = "comic.png"
+
+    with open("config.json", 'r') as file:
+        config = json.load(file)
+
     # Open the original image
     original_image = Image.open(image_path)
 
     # Get monitor size
-    target_size = get_monitors()[0] #gets primary display
+    target_size = get_monitors()[0]  # gets primary display
 
     # Resize the image to fit the specified dimensions
     image_target_size = (target_size.width * max_img_width_pct, target_size.height * max_img_height_pct)
@@ -37,48 +97,59 @@ def composite_image(image_path, max_img_width_pct=0.9, max_img_height_pct=0.75):
     resized_image = original_image.resize((round(original_image.width * image_scale_factor), round(original_image.height * image_scale_factor)), Image.LANCZOS)
 
     # Create a blank white image with the specified resolution
-    background = Image.new('RGB', (target_size.width, target_size.height), (235, 235, 235))
+    background = Image.new('RGBA', (target_size.width, target_size.height), color_from_hex(config["color"]))
 
     # Image offset on the background
-    im_offset = ((target_size.width - resized_image.width) // 2, (target_size.height - resized_image.height) // 2)
+    im_offset = ((target_size.width - resized_image.width) // 2,
+                 (target_size.height - resized_image.height) // 2 + 50)
 
     # Do shadow
-    shadow_offset = (0,15)
-    shadow_opacity = 150
-    blur_radius = 40
+    if config['shadow']:
+        shadow_offset = (0, 15)
+        shadow_opacity = 150
+        blur_radius = 25
 
-    shadow = Image.new('RGBA', background.size, (0, 0, 0, 0))
-    draw = ImageDraw.Draw(shadow)
-    #update the shadow offset to be in the right positon on the bg
-    shadow_offset = add_tuples(shadow_offset, im_offset)
-    draw.polygon([shadow_offset,
-                  (resized_image.width + shadow_offset[0], shadow_offset[1]),
-                  (resized_image.width + shadow_offset[0], resized_image.height + shadow_offset[1]),
-                  (shadow_offset[0], resized_image.height + shadow_offset[1])],
-                 fill=(1, 0, 2, shadow_opacity))
+        shadow = Image.new('RGBA', background.size, (0, 0, 0, 0))
+        draw = ImageDraw.Draw(shadow)
 
-    # for i in range(5):
-    shadow = shadow.filter(ImageFilter.GaussianBlur(radius=blur_radius))
+        # Update the shadow offset to be in the right positon on the bg
+        shadow_offset = add_tuples(shadow_offset, im_offset)
+        draw.polygon([shadow_offset,
+                      (resized_image.width + shadow_offset[0], shadow_offset[1]),
+                      (resized_image.width + shadow_offset[0], resized_image.height + shadow_offset[1]),
+                      (shadow_offset[0], resized_image.height + shadow_offset[1])],
+                     fill=color_from_hex(config["text_color"]) + (shadow_opacity, ))
 
-    background = Image.alpha_composite(background.convert('RGBA'), shadow)
+        shadow = shadow.filter(ImageFilter.GaussianBlur(radius=blur_radius))
+        background = Image.alpha_composite(background, shadow)
 
-    # Paste the resized & shadowed image onto the white background
+    # Write text
+    if config["alt_text"] or config["title"]:
+        # Load the JSON data from the file
+        with open("image_meta.json", 'r') as file:
+            image_meta = json.load(file)
+
+        if config["title"]:
+            draw_text(image_meta["title"], 80, 25, color_from_hex(config["text_color"]), background)
+
+        if config["alt_text"] and not image_meta["title"] == image_meta["alt_text"]:
+            draw_text(image_meta["alt_text"], 20, 135, color_from_hex(config["text_color"]), background)
+
+    # Paste the resized image onto the white background
     background.paste(resized_image, im_offset)
 
     # Save the combined image
-    combined_image_path = "combined_wallpaper.png"
-    background.save(combined_image_path)
+    background.save("combined_wallpaper.png")
 
 def set_background():
     # --- SET BG
     SPI_SETDESKWALLPAPER = 0x0014
 
     # Set the system background
-    ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, os.path.abspath(combined_image_path), 3)
+    ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, os.path.abspath("combined_wallpaper.png"), 3)
 
 
 if __name__ == "__main__":
-    get_image("comic.png")
-
-    composite_image("comic.png")
+    get_image()
+    composite_image()
     set_background()
